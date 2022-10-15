346658eaf6c79a1043b871e7b3176490a5f40e2d
Tinou95 <Quentin95170@hotmail.fr>
2022-10-15 17:33:07 +0200

    map_Aleatoire + modif_Deplacements_Joueurs

--- bomberman.c
+++ bomberman.c
@@ -91,45 +91,22 @@ void placer_murs(int longeur, int largeur, int nb_joueur, char carte[longeur][la
 
 
 
-// Fonction qui place les mur destructibles partout où il n'y a pas de murs indestructibles sauf sur les cases à cotés des joueurs
+// Fonction qui place les mur destructibles partout où il n'y a pas de murs indestructibles au hasard
+
 void placer_murs_destructibles(int longeur, int largeur, int nb_joueur, char carte[longeur][largeur])
 {
-    for (int i = 0; i < longeur; i++)
+    int nb_murs = 0;
+    int nb_murs_max = (longeur * largeur) / 6;
+    int x, y;
+    srand(time(NULL));
+    while (nb_murs < nb_murs_max)
     {
-        for (int j = 0; j < largeur; j++)
+        x = rand() % (longeur - 2) + 1;
+        y = rand() % (largeur - 2) + 1;
+        if (carte[x][y] == ' ')
         {
-            if (i == 0 || i == longeur - 1)
-            {
-                carte[i][j] = 219;
-            }
-            else if (j == 0 || j == largeur - 1)
-            {
-                carte[i][j] = 219;
-            }
-            else if (i == 1 && j == 1 && nb_joueur >= 1)
-            {
-                carte[i][j] = 49;
-            }
-            else if (i == 1 && j == largeur - 2 && nb_joueur >= 2)
-            {
-                carte[i][j] = 50;
-            }
-            else if (i == longeur - 2 && j == 1 && nb_joueur >= 3)
-            {
-                carte[i][j] = 51;
-            }
-            else if (i == longeur - 2 && j == largeur - 2 && nb_joueur >= 4)
-            {
-                carte[i][j] = 52;
-            }
-            else if (i % 2 == 0 && j % 2 == 0)
-            {
-                carte[i][j] = 219;
-            }
-            else if (carte[i][j] != 219 && carte[i][j] != 49 && carte[i][j] != 50 && carte[i][j] != 51 && carte[i][j] != 52)
-            {
-                carte[i][j] = 178;
-            }
+            carte[x][y] = 178;
+            nb_murs++;
         }
     }
 }
@@ -140,9 +117,11 @@ void placer_murs_destructibles(int longeur, int largeur, int nb_joueur, char car
 #define BAS 80
 #define GAUCHE 75
 #define DROITE 77
+#define SPACE 32
 
 
 //Fonction qui pemet de déplacer mon joueur 1 sur la carte en fonction des touches du clavier
+//recuperer la position des mur et mur destructible pour ne pas pouvoir les traverser
 void deplacement_joueur1(int longeur, int largeur, int nb_joueur, char carte[longeur][largeur], int x, int y)
 {
     int touche = 0;
@@ -150,34 +129,36 @@ void deplacement_joueur1(int longeur, int largeur, int nb_joueur, char carte[lon
     switch (touche)
     {
     case HAUT:
-        if (carte[x - 1][y] != 219)
-        {
-            carte[x - 1][y] = 49;
-            carte[x][y] = ' ';
-
-        }
+            if (carte[x - 1][y] == ' ')
+            {
+                carte[x][y] = ' ';
+                x--;
+                carte[x][y] = 49;
+            }
         break;
     case BAS:
-        if (carte[x + 1][y] != 219)
-        {
-            carte[x + 1][y] = 49;
-            carte[x][y] = ' ';
-
-        }
+            if (carte[x + 1][y] == ' ')
+            {
+                carte[x][y] = ' ';
+                x++;
+                carte[x][y] = 49;
+            }
         break;
     case GAUCHE:
-        if (carte[x][y - 1] != 219)
-        {
-            carte[x][y - 1] = 49;
-            carte[x][y] = ' ';
-        }
+            if (carte[x][y - 1] == ' ')
+            {
+                carte[x][y] = ' ';
+                y--;
+                carte[x][y] = 49;
+            }
         break;
     case DROITE:
-        if (carte[x][y + 1] != 219)
-        {
-            carte[x][y + 1] = 49;
-            carte[x][y] = ' ';
-        }
+            if (carte[x][y + 1] == ' ')
+            {
+                carte[x][y] = ' ';
+                y++;
+                carte[x][y] = 49;
+            }
         break;
     }
      system("cls");
@@ -186,8 +167,7 @@ void deplacement_joueur1(int longeur, int largeur, int nb_joueur, char carte[lon
 
 
 
-
-// Fonction qui affiche la carte avec int longeur int largeur et actualisé les déplacements des joueurs
+// Fonction qui affiche la carte avec int longeur int largeur et actualisé les déplacements des joueurs et les bombes
 void afficher_carte(int longeur, int largeur, char carte[longeur][largeur])
 {
     for (int i = 0; i < longeur; i++)
@@ -199,10 +179,6 @@ void afficher_carte(int longeur, int largeur, char carte[longeur][largeur])
         printf("\n");
     }
 }
-
-
-
-
 
 
 
@@ -286,19 +262,19 @@ int main()
             //actualiser les déplacements des joueurs x et y quand le joueur appuie sur les fleches
             if (carte[x - 1][y] == 49)
             {
-                x = x - 1;
+                x -= 1;
             }
             if (carte[x + 1][y] == 49)
             {
-                x = x + 1;
+                x += 1;
             }
             if (carte[x][y - 1] == 49)
             {
-                y = y - 1;
+                y -= 1;
             }
             if (carte[x][y + 1] == 49)
             {
-                y = y + 1;
+                y += 1;
             }
 
         }
